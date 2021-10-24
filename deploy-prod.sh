4e0cd187424fc1aae7a9eef9d1b442f3b1577513
Hau Nguyen <thehaung217@gmail.com>
2021-10-24 11:13:03 +0700

    build: change gitlab for manual build and automation CD

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,2 +1,4 @@
-docker-compose up -d
-docker run -d -p 3000:80 --name frontend-3hq-production 3hq-production
+docker pull registry.gitlab.com/thehaung/fe-3hq-chat
+docker stop frontend-3hq-production
+docker rm frontend-3hq-production
+docker run -d -p 3000:80 --name frontend-3hq-production registry.gitlab.com/thehaung/fe-3hq-chat
