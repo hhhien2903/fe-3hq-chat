25ddc12969584d6ba4ad43f30d66763f33995b29
NhatHoang <phanvonhathoang1122000@gmail.com>
2021-11-07 21:16:17 +0700

    fix: friend request, friend suggestion

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -3,20 +3,19 @@ import ContactList from '../../components/ContactList/ContactList';
 import ContactWindow from '../../components/ContactWindow/ContactWindow';
 import ModalAddFriend from '../../components/ContactWindow/ModalAddFriend';
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar';
-import SuggetionFriendWindow from '../../components/SuggetionFriendWindow/SuggetionFriendWindow';
 import TopLeftBar from '../../components/TopLeftBar/TopLeftBar';
 const Contact = () => {
   return (
     <Row style={{ height: '100vh' }} className="contact">
-      <Col className="sidebar" span={1}>
+      <Col className="sidebar" sm={2} lg={1}>
         <LeftSidebar />
       </Col>
-      <Col className="contact-list-container" span={5}>
+      <Col className="contact-list-container" sm={6} lg={5}>
         <TopLeftBar />
         <ContactList />
         <ModalAddFriend />
       </Col>
-      <Col className="chat-message-container" span={18}>
+      <Col className="chat-message-container" sm={16} lg={18}>
         <ContactWindow />
       </Col>
     </Row>
